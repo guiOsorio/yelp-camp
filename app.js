f3ae062f7c3afa04338b89324d31d651fee8ba8a
guiOsorio <gosorio@sandiego.edu>
2021-06-07 19:17:45 -0700

    added link to herokuapp to app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if(process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
+// https://secret-reaches-62371.herokuapp.com/
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
